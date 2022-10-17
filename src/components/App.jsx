9ac00292de526ef5c19aa6d78dc506d94101c995
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-17 12:10:30 -0400

    Changed js files to jsx, added delete btn to note

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+import AddNote from './AddNote.jsx';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AddNote />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
