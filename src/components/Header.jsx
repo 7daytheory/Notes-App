e9c7cdaff0046a7bad704af6e0a91ffe155ce2d8
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-18 15:27:30 -0400

    Added notice text, updated package.json

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,9 @@ function Header() {
   return (
     <header>
       <h1>Keeper</h1>
+      <p style={{fontStyle: 'italic', fontSize: '12px', marginTop: '10px'}}>
+        Notes will be removed when refreshed so no one can store rude content on my site
+      </p>
     </header>
   );
 }
